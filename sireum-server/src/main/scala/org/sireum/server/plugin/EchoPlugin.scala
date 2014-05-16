b31bc5ada197f151d6ba7a8dc98c578fbcd96d2a
Robby <robby@k-state.edu>
2014-05-16 09:21:25 -0500

    Reorg and added symbologics to Sireum build

---
+++ sireum-server/src/main/scala/org/sireum/server/plugin/EchoPlugin.scala
@@ -0,0 +1,47 @@
+/*
+Copyright (c) 2014 Robby, Kansas State University.        
+All rights reserved. This program and the accompanying materials      
+are made available under the terms of the Eclipse Public License v1.0 
+which accompanies this distribution, and is available at              
+http://www.eclipse.org/legal/epl-v10.html                             
+*/
+
+package org.sireum.server.plugin
+
+import javax.websocket._
+import javax.websocket.server._
+
+import org.sireum.server._
+import org.sireum.util._
+
+/**
+ * @author <a href="mailto:robby@k-state.edu">Robby</a>
+ */
+@ServerEndpoint(value = "/echo/")
+class EchoPlugin extends WebServer.WsPlugin with Logging {
+  override def enabled = false
+  def name = "echo"
+  
+  val eventClass : Class[_] = classOf[EchoPlugin]
+  
+  @OnOpen
+  def onConnect(sess : Session) {
+    logger.info("Socket Connected: " + sess)
+  }
+
+  @OnMessage
+  def onText(message : String) : String = {
+    logger.info("Received TEXT message: " + message)
+    message
+  }
+
+  @OnClose
+  def onClose(reason : CloseReason) {
+    logger.info("Socket Closed: " + reason)
+  }
+
+  @OnError
+  def onError(cause : Throwable) {
+    cause.printStackTrace(System.err)
+  }
+}
