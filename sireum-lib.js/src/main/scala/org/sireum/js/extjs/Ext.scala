52fa285b087d216830060b92bc6d086d13f48d3b
Robby <robby@k-state.edu>
2014-07-08 16:35:38 -0500

    Added Ext.data.NodeInterface.childNodes.

--- sireum-lib.js/src/main/scala/org/sireum/js/extjs/Ext.scala
+++ sireum-lib.js/src/main/scala/org/sireum/js/extjs/Ext.scala
@@ -124,6 +124,7 @@ trait `Ext.data.ArrayStore` extends `Ext.data.Store` {
 
 trait `Ext.data.NodeInterface` extends JsObject {
   def parentNode : JsObject
+  def childeNodes : JsArray[JsObject]
   def sort(sortFn : JsFunction = null, recursive : Boolean = false,
            suppressEvents : Boolean = false)
   def appendChild(
