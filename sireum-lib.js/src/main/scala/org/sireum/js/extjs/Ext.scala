0529be4102e024505a98b2a635e3b9ff7eefe692
JakeEhrlich <jake.h.ehrlich@gmail.com>
2014-09-03 17:40:19 -0500

    Changed findChild to use JsDynamic instead of JsObject

--- sireum-lib.js/src/main/scala/org/sireum/js/extjs/Ext.scala
+++ sireum-lib.js/src/main/scala/org/sireum/js/extjs/Ext.scala
@@ -129,7 +129,7 @@ trait `Ext.data.ArrayStore` extends `Ext.data.Store` {
 trait `Ext.data.NodeInterface` extends JsObject {
   def parentNode : JsObject
   def childeNodes : JsArray[JsObject]
-  def findChild(attr : String, value : JsObject)
+  def findChild(attr : String, value : JsDynamic) : `Ext.data.NodeInterface`
   def sort(sortFn : JsFunction = null, recursive : Boolean = false,
            suppressEvents : Boolean = false)
   def appendChild(
